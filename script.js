b10cbae9e5f041fe1432d03f18e48e86fcfbdb10
Ilan Diaz <idiazjimenez06@gmail.com>
2021-07-14 12:24:56 -0500

    Almacen de computadoras
    html listo
    java iniciando

---
+++ script.js
@@ -0,0 +1,27 @@
+//variables y constantes//
+var total= 0 , cant, descuento, compra;
+const a= 820000
+
+
+//operaciones//
+
+function total() {
+    total = document.getElementById("total").value;
+    total= const a * cant - descuento;
+}
+
+if (cant > 1640000) {
+    var total = compra * 0.15;
+    
+}
+
+
+
+
+
+
+    
+
+
+
+}

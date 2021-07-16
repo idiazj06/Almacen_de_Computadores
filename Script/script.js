b4c54fee3a7dd7365ce504a2615657059f4de121
Ilan Diaz <idiazjimenez06@gmail.com>
2021-07-15 22:05:25 -0500

    Act
    funcion java lista

--- Script/script.js
+++ Script/script.js
@@ -1,27 +1,44 @@
 //variables y constantes//
-var total= 0 , cant, descuento, compra;
-const a= 820000
+let total= 0 ;
+let , cant; 
+let descuento; 
+let compra;
+let totalDesc = 0;
+const compt= 820000
 
 
 //operaciones//
 
+function compra () {
+    compra= document.getElementById("cant").value;
+    compra= compt * cant;
+    
+}
+
 function total() {
     total = document.getElementById("total").value;
-    total= const a * cant - descuento;
+    total= (compt * cant)- descuento;
 }
 
-if (cant > 1640000) {
-    var total = compra * 0.15;
+if (cant <= 3280000 && cant >= 1680000) {
+    descuento = compra * 0.15;    
+}
+else if (cant <= 6560000 && cant > 3280000) {
+    descuento = compra * 0.25;   
+}
+else if (cant <= 9840000 && cant > 6560000) {
+    descuento = compra * 0.35;   
+}
+else if (cant > 9840000) {
+    descuento = compra * 0;   
+}
+else{
+    descuento = compra * 0;
+    alert('El valor ingresado no es válido');
+}
+
     
-}
 
-
-
-
-
-
-    
-
-
-
-}
+document.getElementsById("compra").innerHTML = compra;
+document.getElementsById("descuento").innerHTML = descuento;
+document.getElementsById("total").innerHTML = total;
